4746c14370c43f6921033ed744798a5978232dc6
Alejandro Orrego <alejandro.orrego@crystaldev.cl>
2023-06-25 16:59:07 -0400

    test maps

---
+++ lib/main copy.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _controller = TextEditingController();
+  var uuid = new Uuid();
+
+  List<dynamic> _placeList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      _onChanged();
+    });
+  }
+
+  _onChanged() {
+    getSuggestion(_controller.text);
+  }
+
+  void getSuggestion(String input) async {
+    String kPLACES_API_KEY = "AIzaSyBXG3qvencNar0wXsqtZg8x2nLnNM5FPoY";
+    String type = '(regions)';
+    String _session = '904de719-7a71-49c0-82d4-d990864dab30';
+    String baseURL =
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+    Uri request = Uri.parse(
+        '$baseURL?input=$input&key=$kPLACES_API_KEY&sessiontoken=$_session');
+
+    var response = await http.get(request);
+    if (response.statusCode == 200) {
+      setState(() {
+        _placeList = json.decode(response.body)['predictions'];
+      });
+    } else {
+      throw Exception('Failed to load predictions');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topCenter,
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: "Seek your location here",
+                  focusColor: Colors.white,
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  prefixIcon: Icon(Icons.map),
+                ),
+              ),
+            ),
+            ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: _placeList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_placeList[index]["description"]),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
